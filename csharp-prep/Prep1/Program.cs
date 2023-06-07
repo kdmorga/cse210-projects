735f193912bcf180c8088b3a111910be600f7886
kdmorga <karladanielamorga@gmail.com>
2023-06-07 11:46:20 -0600

    adding first and last name variables

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?") ;
+        string firstname = Console.ReadLine() ;
+
+        Console.WriteLine("What is you Last Name? ") ;
+        string lastname = Console.ReadLine(); 
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname} ") ;
+        
+    
     }
+
+
 }
