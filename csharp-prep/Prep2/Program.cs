8d19371aff6aea9ac0f8471bbd674e0619ac5882
kdmorga <karladanielamorga@gmail.com>
2023-06-08 12:54:20 -0600

    creating letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+
+        if (grade>= 90)
+        {
+            Console.WriteLine("Your grade is an A. You passed the class!");
+        }
+        else if (grade>= 80)
+        {
+            Console.WriteLine("Your grade is a B. You passed the class!");
+        }
+        else if (grade>= 70)
+        {
+            Console.WriteLine("Your grade is a C. You passed the class!");
+        }
+        else if (grade>= 60)
+        {
+            Console.WriteLine("Your grade is a D. You did not pass the class.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F. You did not pass the class.");
+        }
     }
 }
